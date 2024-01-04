ce666b383552068e725eaaea8b1c975d898d415f
Filip <f@filip.world>
2024-01-04 14:12:20 -0500

    merge

--- src/libraries/JB721PermissionIds.sol
+++ src/libraries/JB721PermissionIds.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-<<<<<<< HEAD
 /// @notice Permission IDs for `JBPermissions`.
 library JB721PermissionIds {
     // 1-20 - `JBPermissionIds`
@@ -9,15 +8,4 @@ library JB721PermissionIds {
     uint256 public constant ADJUST_TIERS = 22;
     uint256 public constant UPDATE_METADATA = 23;
     uint256 public constant MINT = 24;
-=======
-/// @title JBBitJB721Operationsmap
-/// @notice Occupy namespace operations.
-library JB721Operations {
-    // 0...18 - JBOperations
-    // 19 - JBOperations2 (ENS/Handle)
-    // 20 - JBUriOperations (Set token URI)
-    uint256 public constant ADJUST_TIERS = 21;
-    uint256 public constant UPDATE_METADATA = 22;
-    uint256 public constant MINT = 23;
->>>>>>> intermediate
 }
